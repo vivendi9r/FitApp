ff3807621b2284b75b536ff83cfbccab0bad7ecc
vivendi9r <vivendi9r@gmail.com>
2018-07-12 21:07:31 +0200

    dodanie kontekstu - ActivityContext

---
+++ FitApp/FitApp/DataAccessLayer/ActivityContext.cs
@@ -0,0 +1,17 @@
+﻿using FitApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FitApp.DataAccessLayer
+{
+    public class ActivityContext : DbContext
+    {
+        public ActivityContext() : base("DefaultConnection")
+            { }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
