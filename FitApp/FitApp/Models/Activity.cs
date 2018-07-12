756a28ad1b1455f7f6b59e533975900e0bb2d630
vivendi9r <vivendi9r@gmail.com>
2018-07-12 20:56:27 +0200

    uzupelnienie klasy Activity

--- FitApp/FitApp/Models/Activity.cs
+++ FitApp/FitApp/Models/Activity.cs
@@ -7,5 +7,12 @@ namespace FitApp.Models
 {
     public class Activity
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Start_time { get; set; }
+        public DateTime End_time { get; set; }
+        public string Room { get; set; }
+        public string Coach { get; set; }
+        public List<string> Clients_list { get; set; }
     }
 }
