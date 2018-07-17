3abbc697c9870de8d6cd1cee2f0eae0266937595
vivendi9r <vivendi9r@gmail.com>
2018-07-17 07:50:35 +0200

    dodanie tworzenia treningu : Activity/Create

---
+++ FitApp/FitApp/Controllers/ActivityController.cs
@@ -0,0 +1,41 @@
+﻿using FitApp.DataAccessLayer;
+using FitApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitApp.Controllers
+{
+    public class ActivityController : Controller
+    {
+        // GET: Activity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Activity activity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", activity);
+            }
+            else
+            {
+                ActivityContext db = new ActivityContext();
+                db.Activities.Add(activity);
+                db.SaveChanges();
+
+                return View("Index");
+            }
+        }
+    }
+}
